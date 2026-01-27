78da161069fdf9b069edbda9c8613890db3c0ee3
mensylisir <710080675@qq.com>
2026-01-27 16:01:19 +0800

    Fix syntax error in kubexm.go

--- installer/internal/algo/kubexm.go
+++ installer/internal/algo/kubexm.go
@@ -10,6 +10,7 @@ import (
 	"html/template"
 )
 
+const (
 	// ImgpkgVersion defines the imgpkg version that will be installed on host if imgpkg is not already installed
 	ImgpkgVersion = "v0.36.4"
 )
